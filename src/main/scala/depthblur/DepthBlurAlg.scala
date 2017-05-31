be310ac95d551f541e429096ea47f3bf3bc8a3a8
Gogo Lejzr <gogo.lejzr@gmail.com>
2017-05-31 16:26:27 +0200

    basic box filter (no depth adjustment)

--- src/main/scala/depthblur/DepthBlurAlg.scala
+++ src/main/scala/depthblur/DepthBlurAlg.scala
@@ -4,10 +4,19 @@ import scala.collection.mutable.ArrayBuffer
 import scalafx.scene.image.{Image, WritableImage, PixelReader, PixelWriter}
 import javafx.scene.image.{WritablePixelFormat, PixelFormat}
 
-
 object DepthBlurAlg {
+
+	private def getAverage(pixels: Array[Int]) : Int = 
+	{
+		val a = 0xFF
+		val rAvg = (pixels.map(p => (p >> 16) & 255).sum) / pixels.length
+		val gAvg = (pixels.map(p => (p >> 8) & 255).sum) / pixels.length
+		val bAvg = (pixels.map(p => p & 255).sum) / pixels.length
+
+		return (a << 24) + (rAvg << 16) + (gAvg << 8) + bAvg
+	}
+
     def boxFilter(x: Int, y: Int, img: Image, dpt: Image) : WritableImage = {
-        println(s"test: $x $y")
 
         val w = img.width.toInt
         val h = img.height.toInt
@@ -17,20 +26,27 @@ object DepthBlurAlg {
         val imgReader = img.getPixelReader
         val dptReader = dpt.getPixelReader
 
+        val target_depth = dptReader.getColor(x,y).getRed
+
+        println(s"box filter: target depth[$x $y] = $target_depth")
+
         val writer = res.getPixelWriter
+        val format = PixelFormat.getIntArgbInstance()
 
-        for(i <- 0 to (h - 1)){
-        	for(j <- 0 to (w - 1))
+        
+        for(i <- 7 to (h - 8)){
+        	for(j <- 7 to (w - 8))
         	{
-        		//val pixBuffer = new Array[Byte](3)
-        		//imgReader.getPixels(i, j, 1, 1, format, pixBuffer, 1, 1)
-        		//println(pixBuffer)
-        		val ip = imgReader.getArgb(j, i)
-        		val dp = dptReader.getArgb(j, i)
-
-        		writer.setArgb(j, i, ip + dp)
+        		val pixBuffer = new Array[Int](13*13)
+        		imgReader.getPixels(j-7, i-7, 13, 13, format, pixBuffer, 0, 13)
+        		val new_pix = getAverage(pixBuffer).toInt
+        		writer.setArgb(j,i,new_pix)
         	}
         }
+
+        //val pixBuffer = new Array[Int](50*50)
+        //imgReader.getPixels(x-25, y-25, 50, 50, format, pixBuffer, 0, 50)
+        //writer.setPixels(x-25,y-25,50,50,format,pixBuffer,0,50)
 
         return res
     }
